f54cd32bd4dde6fcf9a72f72ba8b90e77cfb4179
unknown <khandvemukesh@gmail.com>
2026-01-14 19:13:37 +0530

    Seconf Fix Vercel build env

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import { Product } from '@/types/product';
 
-const API_BASE_URL = 'https://fakestoreapi.com';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://fakestoreapi.com';
 
 export async function getAllProducts(): Promise<Product[]> {
   const response = await fetch(`${API_BASE_URL}/products`, {
